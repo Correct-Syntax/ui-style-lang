dfbd5f7443ef33f5d2630d7c3d2996e74ad0ad2e
Correct Syntax <correctsyntax@yahoo.com>
2020-08-07 11:43:22 -0500

    add new custom widget demo

---
+++ demo/demo3.py
@@ -0,0 +1,114 @@
+# Demo for UI Style Lang
+# ======================
+
+# This demo file (demo3.py) is MIT licensed - (C) 2020 Noah Rahm, Correct Syntax
+
+# Usage:
+# It's a custom button...click it!
+
+# TODO:
+# Comments!!!
+
+
+import wx
+
+from uistylelang import UIStylePDC, UIStyleApp, UIStyleFrame
+
+
+class MainApplication(UIStyleFrame):
+    def __init__(self):
+        UIStyleFrame.__init__(self, None, title="UI Style Lang Demo", pos=(0, 0), size=(1000, 800), name="main-frame")
+
+        # Create the DC
+        self._pdc = UIStylePDC(
+            self,
+            './demo3-styles.uiss'
+            ) 
+
+        self.DrawDrawing(self._pdc)
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, lambda x: None)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+        self.Bind(wx.EVT_MOTION, self.OnMotion)
+
+
+    def OnPaint(self, event):
+        dc = wx.BufferedPaintDC(self)
+        dc = wx.GCDC(dc)
+
+        # We need to clear the dc BEFORE calling PrepareDC.
+        dc.Clear()
+
+        # Draw to the dc using the calculated clipping rect.
+        self._pdc.DrawToDCClipped(
+            dc, wx.Rect(0, 0, self.Size[0], self.Size[1])
+            )
+
+    def OnMotion(self, event):
+        pnt = event.GetPosition()
+        elem_rect = self._pdc.GetWXRect('button')
+        mouse_pnt = wx.Rect(pnt[0], pnt[1], 1, 1)
+        
+        if mouse_pnt.Intersects(elem_rect) == True:
+            self._pdc.UpdateShapeStyles('button', styles="background-color: #FDFDFD;")
+        else:
+            self._pdc.UpdateShapeStyles('button', styles="background-color: #F4F4F4;")
+
+        self.RefreshDemo()
+
+
+    def OnLeftDown(self, event):
+        pnt = event.GetPosition()
+        elem_rect = self._pdc.GetWXRect('button')
+
+        # Create a 1x1 rect for the mouse pointer
+        mouse_pnt = wx.Rect(pnt[0], pnt[1], 1, 1) 
+        
+        if mouse_pnt.Intersects(elem_rect) == True:
+            self._pdc.UpdateShapeStyles('button', styles="background-color: #D1D1D1;")
+
+        self.RefreshDemo()
+
+    def OnLeftUp(self, event):
+        pnt = event.GetPosition()
+        elem_rect = self._pdc.GetWXRect('button')
+
+        # Create a 1x1 rect for the mouse pointer
+        mouse_pnt = wx.Rect(pnt[0], pnt[1], 1, 1) 
+        
+        if mouse_pnt.Intersects(elem_rect) == True:
+            self._pdc.UpdateShapeStyles('button', styles="background-color: #F4F4F4;")
+            self.ButtonCallback()
+            
+        self.RefreshDemo()
+
+    def DrawDrawing(self, dc):
+
+        # Initial
+        dc.InitShapeStyles('button')
+        dc.InitTextStyles('button-text', text="UI Style Lang Demo")
+
+
+    def RefreshDemo(self):
+        rect = wx.Rect(0, 0, self.Size[0], self.Size[1])
+        self.RefreshRect(rect, False)
+        self.Refresh()
+
+    def ButtonCallback(self):
+        notify = wx.adv.NotificationMessage(
+            title="Button Clicked",
+            message="You clicked the UI Style Lang custom button",
+            parent=None, flags=wx.ICON_INFORMATION)
+        notify.Show(timeout=2) # 1 for short timeout, 100 for long timeout
+
+
+
+if __name__ == '__main__':
+    # Create the app and startup
+    app = UIStyleApp(file='./custom-widget-demo.uiss', redirect=False)
+    frame = MainApplication()
+    frame.Show()
+    app.SetTopWindow(frame)
+    app.MainLoop()
