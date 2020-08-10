d8369073ecd37592fba1390b47553882057bd456
Correct Syntax <correctsyntax@yahoo.com>
2020-08-10 07:51:19 -0500

    renamed file

--- uistylelang/__init__.py
+++ uistylelang/__init__.py
@@ -1,5 +1,5 @@
 from .context import UIStylePDC
-from .frame import UIStyleApp, UIStyleFrame, UIStylePanel
+from .widgets import UIStyleApp, UIStyleFrame, UIStylePanel
 from .lang import UIStyleLangParser
 from .utils import ReadRawFile, MergeParsedStyles
 
