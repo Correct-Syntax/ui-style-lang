7d8076edbb1c3cceca88858876f75394f80470a3
Correct Syntax <correctsyntax@yahoo.com>
2020-08-10 06:50:40 -0500

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Noah Rahm, Correct Syntax'
 author = 'Noah Rahm, Correct Syntax'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
