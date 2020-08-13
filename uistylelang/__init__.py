e62fd602333a57bd53d5f4cf8a70159e3eaa44b9
Correct Syntax <correctsyntax@yahoo.com>
2020-08-13 13:48:10 -0500

    added ``UIStyleStaticText`` to API

--- uistylelang/__init__.py
+++ uistylelang/__init__.py
@@ -1,5 +1,4 @@
 from .context import UIStylePDC
-from .widgets import UIStyleApp, UIStyleFrame, UIStylePanel
+from .widgets import UIStyleApp, UIStyleFrame, UIStylePanel, UIStyleStaticText
 from .lang import UIStyleLangParser
 from .utils import ReadRawFile, MergeParsedStyles
-
